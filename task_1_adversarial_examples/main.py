6f7808cfdaca12bdd4a629246421d391804c8728
IvoHoese <ivho00001@stud.uni-saarland.de>
2025-11-20 10:38:15 +0100

    Adjusted Port

--- task_1_adversarial_examples/main.py
+++ task_1_adversarial_examples/main.py
@@ -104,7 +104,7 @@ Let `G` be the ground-truth tensor loaded:
   → Clean your array: `np.isfinite(images).all()` must be True.
 """
 
-BASE_URL  = "http://34.122.51.94:9000"
+BASE_URL  = "http://34.122.51.94:80"
 API_KEY  = "YOUR_API_KEY_HERE"  
 
 TASK_ID = "10-adversarial-examples"
